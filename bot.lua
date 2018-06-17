6fe4353b1d2bfaf06578ccbb129b4a21bd6af7cd
SuperEvilAzmil <hakim.azmil55@yahoo.com>
2018-06-17 23:01:48 +0800

    Summary

--- bot.lua
+++ bot.lua
@@ -46,7 +46,7 @@ end)
 Client:on('typingStart', function(UserId,ChannelId,Timestamp)
 	print('Channel: ' .. ChannelId)
 	print('Time: ' .. Timestamp)
-	ChannelId.id:send('Rape')
+	ChannelId:send('Rape')
 end)
 
 Client:run('Bot NDU3ODMxNjM5MDYyNjc1NDU3.Dge35w.a3tt3pRXrrRl9qe5IyH4GoPz-54')
